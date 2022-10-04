0c4e70a4704cbe1e077c3e4dcdf2038a36f9adda
snehagarwal <snehagarwal@microsoft.com>
2022-10-04 13:02:45 -0700

    Added deny list generation with pandas for perf optimization

--- bing_spam_integration.py
+++ bing_spam_integration.py
@@ -1,9 +1,10 @@
 import csv
+import pandas as pd
 
 # formats:
-# bing spam list: google.com
-# Deny list: www.google.com
-# Telemetry: com.google.www 
+# bing spam list: google.com (does not have www)
+# Deny list: www.google.com 
+# Telemetry: com.google.www (reversed)
 
 # intersect function to check which subdomains in the Bing spam are present in the Edge Permissions telemetry and
 # create a new list.
@@ -14,17 +15,17 @@ def FindNotificationSendingDomainsInBingSpamList(bing_spam_domains_csv_file_path
         csv_reader_policy = csv.reader(csv_file_policy, delimiter=",")
 
         for domain_row in csv_reader_policy:
-            print(f"FindNotificationSendingDomainsInBingSpamList: Reading bing spam list domain: {domain_row}")
+            #print(f"FindNotificationSendingDomainsInBingSpamList: Reading bing spam list domain: {domain_row}")
             sub_domain_name = domain_row[0]
             eTLD_name = domain_row[1]
             # check if the sub domain is  == to eTLD in bing spam list
             if sub_domain_name == eTLD_name and DomainExists(sub_domain_name, edge_notification_sending_domains_dict):
-                print(f"FindNotificationSendingDomainsInBingSpamList: Found Spammy Notification Sending Domain: {sub_domain_name}")
+                #print(f"FindNotificationSendingDomainsInBingSpamList: Found Spammy Notification Sending Domain: {sub_domain_name}")
                 notification_sending_domains_in_bing_dict[sub_domain_name] = 1
-    
+    print("FindNotificationSendingDomainsInBingSpamList: notification_sending_domains_in_bing_dict created ")
     return notification_sending_domains_in_bing_dict
 
-# create a dictioary with the orgins in the edge permission telemtry to make it less expensive to read. 
+# create a dictionary with the orgins in the edge permission telemetry to make it less expensive to read. 
 def ReadNotificationSendingDomainsFromEdgeTelemetry(edge_telemetry_csv_file_path):
     edge_notification_sending_domains_dict = {}
 
@@ -32,15 +33,15 @@ def ReadNotificationSendingDomainsFromEdgeTelemetry(edge_telemetry_csv_file_path
         csv_reader_policy = csv.reader(csv_file_policy, delimiter=",")
 
         for domain_row in csv_reader_policy:
-            print(f"ReadNotificationSendingDomainsFromEdgeTelemetry: Reading Edge Telemetry Domain: {domain_row}")
+            #print(f"ReadNotificationSendingDomainsFromEdgeTelemetry: Reading Edge Telemetry Domain: {domain_row}")
             origin = domain_row[0]
             edge_notification_sending_domains_dict[origin] = 1
+    
+    print("ReadNotificationSendingDomainsFromEdgeTelemetry: edge_notification_sending_domains_dict created ")
     return  edge_notification_sending_domains_dict
-
 
 # union function : to add these bing spam domains in the deny_list if it doesn't already exist.
 def MergeSpamDomainNamesInDenyList(deny_list_file_path, notification_sending_domains_in_bing_dict):
-
     current_deny_list_domains_dict = {}
 
     # cache all existing domains in the current deny list into a dict
@@ -48,7 +49,7 @@ def MergeSpamDomainNamesInDenyList(deny_list_file_path, notification_sending_dom
         csv_reader_policy = csv.reader(csv_file_policy)
         
         for deny_list_domain in csv_reader_policy:
-            print(f"MergeSpamDomainNamesInDenyList: Reading Deny List Domain: {deny_list_domain}")
+            #print(f"MergeSpamDomainNamesInDenyList: Reading Deny List Domain: {deny_list_domain}")
 
             domain_name = deny_list_domain[0]
             current_deny_list_domains_dict[domain_name] = 1
@@ -57,12 +58,12 @@ def MergeSpamDomainNamesInDenyList(deny_list_file_path, notification_sending_dom
     with open(deny_list_file_path, mode='a+', encoding="utf8") as dentListTextFile:
 
         for spam_notification_domain in notification_sending_domains_in_bing_dict:
-            print(f"MergeSpamDomainNamesInDenyList: Reading Spammy Notification sending Domain: {spam_notification_domain}")
+            #print(f"MergeSpamDomainNamesInDenyList: Reading Spammy Notification sending Domain: {spam_notification_domain}")
 
             if not DomainExists(spam_notification_domain, current_deny_list_domains_dict):
-                print(f"MergeSpamDomainNamesInDenyList: Adding new domain to current deny list: {spam_notification_domain}")
+                #print(f"MergeSpamDomainNamesInDenyList: Adding new domain to current deny list: {spam_notification_domain}")
                 dentListTextFile.write(f"\n{spam_notification_domain}")
-
+    print(f"MergeSpamDomainNamesInDenyList: Added new domain to current deny list")
 
 def DomainExists(domain_name, dict):
     for item in dict:
@@ -92,4 +93,4 @@ deny_list_file_path = 'production_domains.txt'
 
 edge_notification_sending_domains_dict = ReadNotificationSendingDomainsFromEdgeTelemetry(edge_telemetry_file_path)
 notification_sending_domains_in_bing_dict = FindNotificationSendingDomainsInBingSpamList(bing_spam_list_file_path, edge_notification_sending_domains_dict)
-MergeSpamDomainNamesInDenyList(deny_list_file_path, notification_sending_domains_in_bing_dict)
+MergeSpamDomainNamesInDenyList(deny_list_file_path, notification_sending_domains_in_bing_dict)  
---
+++ denylist_with_pandas.py
@@ -0,0 +1,65 @@
+import pandas as pd
+
+def NormalizeDomain(domain1):
+    #print(f"NormalizeDomain:{domain1}")
+    
+    #convert ints and floats to string in case there are IP addresses as  
+    if(str(domain1) == ""):
+        return ""
+
+    domain1_parts = str(domain1).split(".")
+    domain1_parts.sort()
+
+    if ("www" in domain1_parts):
+        domain1_parts.remove("www")
+    
+    normalized_domain_string = '.'.join(domain1_parts)
+    return normalized_domain_string  
+
+def PrintDataframe(df):
+    for index, row in df.iterrows():
+        print(row)
+
+
+# Work with Bing Spam list, normalize the domains and print.
+spam_list_df = pd.read_csv('all_bing_spam_domains.csv', usecols=['SpamDomain', 'etldDomain'])
+spam_list_df['NormalizedDomain'] = spam_list_df['SpamDomain'].map(NormalizeDomain)
+print(f"Size of Span list data frame: {spam_list_df.size}")
+#PrintDataframe(df)
+
+# Work with Edge telemetry, normalize and print.
+telemetry_df = pd.read_csv('edge-permission-telemetry.csv', usecols=['Origin'])
+telemetry_df['NormalizedDomain'] = telemetry_df['Origin'].map(NormalizeDomain)
+print(f"Size of Telemetry data frame: {telemetry_df.size}")
+
+# Merge the normalized Bing Spam List with Edge telemetry using inner join.
+merged_df = pd.merge(spam_list_df, telemetry_df, how='inner', on=['NormalizedDomain'])
+merged_df.drop_duplicates() # removes dupes
+print(f"Printing Merged DataFrame: Spammy Notification Domains")
+#PrintDataframe(merged_df)
+print(f"Spammy Notification Domains Size: {merged_df.size}")
+
+# Read the deny list.
+print("Read deny list dataframe")
+deny_list_df = pd.read_csv('production_domains.txt', names=["SpamDomain"])
+deny_list_df.drop_duplicates()
+
+# Update deny list with spammy notification domains
+print("Concatenate: Deny list data frame + Spammy notificaton domains")
+frames_to_concat = [ deny_list_df, merged_df[["SpamDomain"]] ] # put the frames to union in a array
+concat_deny_list_df = pd.concat(frames_to_concat)
+concat_deny_list_df.drop_duplicates() # remove dupes if any
+#PrintDataframe(concat_deny_list_df)
+print(f"Final Deny List DataFrame size: {concat_deny_list_df.size}")
+
+# Write final deny list to file.
+concat_deny_list_df.to_csv("final_deny_list_domains.csv", index=False, header=None)
+
+
+
+
+
+
+
+
+  
