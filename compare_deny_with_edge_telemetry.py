c3f616a3a3ca81dd2f508585f608629ae638d8f2
snehagarwal <snehagarwal@microsoft.com>
2022-10-06 18:25:57 -0700

    corrected names and print statements

--- compare_deny_with_edge_telemetry.py
+++ compare_deny_with_edge_telemetry.py
@@ -25,21 +25,21 @@ def PrintDataframe(df):
     for index, row in df.iterrows():
         print(row)
 
-# Find intersection of deny with the google telemtry data 
+# Find intersection of deny with the Edge permissions telemtry data 
 def edge_deny_intersection(edge_df, deny_df):
     merged_df = pd.merge(deny_df, edge_df, how='inner', on=['NormalizedDomain'])
     merged_df.drop_duplicates() # removes dupes
-    print(f"intersection of chrome and deny list Domains Size: {len(merged_df.axes[0])}")
-    merged_df.to_csv("chrome_deny_intersect_only_domains.csv", index=False, columns =['NormalizedDomain', 'Origin'])
+    print(f"intersection of Edge and deny list Domains Size: {len(merged_df.axes[0])}")
+    merged_df.to_csv("Edge_deny_intersect_only_domains.csv", index=False, columns =['NormalizedDomain', 'Origin'])
     return merged_df
 
 # Main Program 
 
 # Work with Edge telemetry, normalize and print.
 telemetry_df = pd.read_csv('edge_telemetry_all_domains.csv', usecols=['Origin'])
-print(f"Size of Telemetry data frame: {len(telemetry_df.axes[0])}")
+print(f"Size of Edge Telemetry data frame: {len(telemetry_df.axes[0])}")
 telemetry_df['NormalizedDomain'] = telemetry_df['Origin'].map(NormalizeDomain)
-print(f"Size of Telemetry data frame: after normalization {len(telemetry_df.axes[0])}")
+print(f"Size of Edge Telemetry data frame: after normalization {len(telemetry_df.axes[0])}")
 
 # Read the deny list.
 print("Read deny list dataframe")
