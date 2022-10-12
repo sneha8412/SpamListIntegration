cb6927c9bec39c9d530361a9c317c2a132c672b8
snehagarwal <snehagarwal@microsoft.com>
2022-10-12 13:16:47 -0700

    comments updated

--- bing_spam_and_edge_tele_and_denylist_with_pandas.py
+++ bing_spam_and_edge_tele_and_denylist_with_pandas.py
@@ -36,29 +36,29 @@ start_time = time.time()
 # Work with Edge telemetry, normalize and print.
 telemetry_df = pd.read_csv('edge_telemetry_all_domains.csv', usecols=['Origin'])
 telemetry_df['NormalizedDomain'] = telemetry_df['Origin'].map(NormalizeDomain)
-print(f"Size of Telemetry data frame after normalization: {len(telemetry_df.axes[0])}")
+print(f"Size of Edge Telemetry data frame after normalization: {len(telemetry_df.axes[0])}")
 
 # Work with Bing Spam list, normalize the domains and print.
 spam_list_df = pd.read_csv('all_bing_spam_domains.csv', usecols=['SpamDomain', 'etldDomain', 'SpamLabel'])
 print(f"Size of Spam list data frame: {len(spam_list_df.axes[0])}")
 spam_list_df['NormalizedDomain'] = spam_list_df['SpamDomain'].map(NormalizeDomain)
-print(f"Size of Spam list data frame: after normalization {len(spam_list_df.axes[0])}")
+#print(f"Size of Spam list data frame: after normalization {len(spam_list_df.axes[0])}")
 
 # Get the domains from the bing spam that have Spam and Crush Label only. 
 result_spam_df = spam_list_df.query("SpamLabel == 'Spam'")
-print(f"size of domains with spam label {len(result_spam_df.axes[0])}")
+print(f"size of domains with spam label in Bing Spam List {len(result_spam_df.axes[0])}")
 result_crush_df = spam_list_df.query("SpamLabel == 'Crush'")
-print(f"size of domains with Crush label {len(result_crush_df.axes[0])}")
+print(f"size of domains with Crush label in bing Spam list {len(result_crush_df.axes[0])}")
 df_to_concat = [ result_spam_df, result_crush_df[["SpamDomain"]] ]
 result_df = pd.concat(df_to_concat) # gets all rows with "Spam" label
-print(f"size of the result of domains with spam and crush combined labels {len(result_df.axes[0])} ")
+print(f"size of the result of domains with spam and crush combined labels in bing Spam list {len(result_df.axes[0])} ")
 
 # Merge the normalized Bing Spam List with Edge telemetry using inner join.
 merged_df =  pd.merge(result_df, telemetry_df, how='inner', on=['NormalizedDomain'])
 merged_df.drop_duplicates(inplace=True) # removes dupes
 merged_df["SpamDomain"].to_csv("bing_spam_domains_from_edge_tobe_added_to_denylist.csv", index=False, header=['origin'])
 merged_df.to_csv("bing_spammy_domains_in_edge_telemetry.csv", index=False)
-print(f"Spammy Notification Domains Size with Crush and Spam label: {len(merged_df.axes[0])}")
+print(f"size of Spammy Notification Domains found in Edge telemetry: {len(merged_df.axes[0])}")
 
 # Read the deny list.
 print("Read deny list dataframe")
@@ -67,7 +67,7 @@ deny_list_df.drop_duplicates(inplace=True)
 print(f"Size of existing DenyList data frame: {len(deny_list_df.axes[0])}")
 
 # Update deny list with spammy notification domains
-print("Concatenate: Deny list data frame + Spammy notificaton domains")
+print("Concatenate: Existing Deny list data frame + New Spammy notificaton domains")
 frames_to_concat = [ deny_list_df, merged_df[["SpamDomain"]] ] # put the frames to union in a array
 concat_deny_list_df = pd.concat(frames_to_concat)
 concat_deny_list_df.drop_duplicates(inplace=True) # remove dupes if any
